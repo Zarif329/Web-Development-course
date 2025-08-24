9bbb2e7da86bee82a1b7041b97bada29086896de
Zarif329 <kmzarif28@gmail.com>
2025-08-24 10:05:58 -0700

    Add files via upload

---
+++ Web-Dev-Codingal/ACP/ACP/pythone/number swaping/numberswaping.py
@@ -0,0 +1,13 @@
+
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+c = int(input("Enter third number (c): "))
+
+print("\nBefore swapping:")
+print("a =", a, "b =", b, "c =", c)
+
+
+a, b, c = b, c, a
+
+print("\nAfter swapping:")
+print("a =", a, "b =", b, "c =", c)
